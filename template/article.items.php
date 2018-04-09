ad4e8c351b2523a82a9caa0f5f85bc7e7a5a409a
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-04-09 18:20:37 +0700

    Article items style fix

--- template/article.items.php
+++ template/article.items.php
@@ -1,12 +1,27 @@
 <article class="article-items">
-	<header>
-		<h2><a href="article/<?php echo $var['id'];?>/<?php echo $var['url'];?>"><?php echo (!empty($var['title'])?$var['title']:'Untitle');?></a></h2>
-	</header>
-	<p class="info">
-		<?php if($var['status'] == 'published'){?>
-		<span><?php echo $var['category_title'];?></span> · <span><?php echo (!empty($var['edit_time'])?$var['edit_time']:$var['create_time']);?></span>
+	<figure class="thumbnail">
+		<?php if(!empty($var['cover_id'])){?>
+		<a href="article/<?php echo $var['id'];?>/<?php echo $var['url'];?>">
+			<img src="image/upload/<?php echo $var['id'];?>/square/<?php echo $var['cover_img'];?>" alt="">
+		</a>
 		<?php }else{?>
-		<span>ฉบับร่าง</span>
+		<i class="fal fa-file-alt"></i>
 		<?php }?>
-	</p>
+	</figure>
+	<div class="content">
+		<header>
+			<h2><a href="article/<?php echo $var['id'];?>/<?php echo $var['url'];?>"><?php echo (!empty($var['title'])?$var['title']:'Untitle');?></a></h2>
+		</header>
+		<p class="info">
+			<?php if($var['status'] == 'published'){?>
+			<span><i class="fal fa-check-circle"></i> · <?php echo $var['category_title'];?></span> · <span><?php echo (!empty($var['edit_time'])?$var['edit_time']:$var['create_time']);?></span>
+			<?php }else{?>
+			<span><i class="fal fa-lock-alt"></i> · ฉบับร่าง</span>
+			<?php }?>
+		</p>
+	</div>
+	<div class="stat">
+		<div class="v">0</div>
+		<div class="c">Read</div>
+	</div>
 </article>
