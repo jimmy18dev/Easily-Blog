cd8162e5590b81c2a1cac927ae3e3189fbd0875e
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-04-12 16:32:21 +0700

    Profile button

--- template/header.profile.php
+++ template/header.profile.php
@@ -1,3 +1,4 @@
-<a class="btn btn-profile" href="profile">
+<a class="btn-profile" href="profile">
 	<img src="<?php echo (empty($user->fb_id)?'image/avatar.png':'https://graph.facebook.com/'.$user->fb_id.'/picture?type=square');?>" alt="Profile avatar">
+	<span><?php echo $user->fullname;?></span>
 </a>
