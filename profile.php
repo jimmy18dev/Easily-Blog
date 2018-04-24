c68d9316f81ae57872d03f7f9df5367e5696c132
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-04-24 18:21:26 +0700

    Profile page redesign.

--- profile.php
+++ profile.php
@@ -37,18 +37,6 @@ $c_article 	= $article->counter($user->id);
 <body>
 <header class="header">
 	<a class="btn left" href="index.php"><i class="fal fa-arrow-left" aria-hidden="true"></i><span>หน้าแรก</span></a>
-
-    <span class="center">โปรไฟล์</span>
-
-	<?php if($user_online){?>
-	<a class="btn right" href="article/create"><i class="fal fa-plus"></i><span>เขียนบทความ</span></a>
-	<?php }else{?>
-	<a href="signin" class="btn"><span>ลงชื่อเข้าใช้</span><i class="fa fa-angle-right" aria-hidden="true"></i></a>
-	<?php }?>
-
-	<?php if(!empty($article->id) && $article->owner_id == $user->id){?>
-	<a href="article/<?php echo $article->id;?>/editor" class="btn iconleft"><span>แก้ไขบทความ</span><i class="fa fa-cog" aria-hidden="true"></i></a>
-	<?php }?>
 </header>
 
 <div class="pagehead">
@@ -56,18 +44,25 @@ $c_article 	= $article->counter($user->id);
 		<figure class="avatar">
 			<img src="<?php echo (empty($user->fb_id)?'image/avatar.png':'https://graph.facebook.com/'.$user->fb_id.'/picture?type=large');?>" alt="Profile avatar">
 		</figure>
+
+        <div class="control">
+            <a href="signout" title="ออกจากระบบ"><i class="fal fa-sign-out"></i></a>
+            <a href="profile/edit">แก้ไขโปรไฟล์</a>
+        </div>
 		<div class="info">
-			<h1><?php echo $user->fullname;?></h1>
-            <?php if(!empty($user->display)){ ?>
-            <p class="fullname"><strong>ชื่อที่แสดง:</strong> <?php echo $user->display;?></p>
-            <?php }?>
-			<p><?php echo $user->bio;?> · <a href="profile/edit">แก้ไข<i class="fal fa-angle-right"></i></a></p>
+			<h1><?php echo ((!empty($user->display))?$user->display:$user->fullname);?></h1>
+			<p><?php echo $user->bio;?></p>
 		</div>
 	</div>
 </div>
+
 <div class="navi">
     <a href="profile" class="active">บทความ</a>
-    <a href="signout" class="right">ออกจากระบบ</a>
+    <a href="profile">หมวดหมู่</a>
+</div>
+
+<div class="filter">
+    <a class="btn-create" href="article/create">เขียนบทความ</a>
 </div>
 
 <div class="article-list">
@@ -83,10 +78,12 @@ $c_article 	= $article->counter($user->id);
 
 <script type="text/javascript" src="js/lib/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="js/init.js"></script>
+<script type="text/javascript" src="js/lib/tippy.all.min.js"></script>
 <script type="text/javascript" src="js/lib/progressbar.js"></script>
 <script type="text/javascript">
 $(function(){
     var progressbar = $('#progressbar');
+    tippy('[title]',{arrow: true});
 
 	// Article Sticky
     $('.btn-sticky').click(function(){
