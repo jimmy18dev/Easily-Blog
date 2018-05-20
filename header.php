cac6c007b9febb037b71a90e975eb31857e5b4bd
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-05-20 14:23:32 +0700

    Fix Change logo position and set vertical-align middle

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <header class="header <?php echo (!empty($article->head_cover_img)?'transparent':'');?>">
-	<div class="logo">
-		<a href="index.php">
-			<img src="image/logo.png" alt="">
-			<span><?php echo $config['settings']['sitename_th'];?></span>
-		</a>
-	</div>
+
+	<div class="btn-icon"><i class="fal fa-star"></i></div>
+
+	<a href="index.php" class="logo">
+		<img src="image/logo.png" alt="">
+		<span><?php echo $config['settings']['sitename_th'];?></span>
+	</a>
 
 	<?php if($current_page == 'home' || $current_page == 'articles'){?>
 	<div class="btn-icon right" id="btn-category"><i class="fal fa-bars"></i></div>
