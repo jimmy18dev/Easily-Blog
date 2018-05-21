c25d17e9552f0f0919e2484b8917de0d958131ef
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-05-21 15:39:59 +0700

    Fix Search box in iPhone layout

--- header.php
+++ header.php
@@ -1,15 +1,16 @@
 <header class="header <?php echo (!empty($article->head_cover_img)?'transparent':'');?>">
+	<?php if($current_page == 'home' || $current_page == 'articles'){?>
+	<div class="btn-icon" id="btn-category"><i class="fal fa-bars"></i></div>
+	<?php }?>
 
-	<div class="btn-icon"><i class="fal fa-star"></i></div>
+	<?php if($current_page == 'article'){?>
+	<a href="index.php" class="btn-icon" id="btn-back"><i class="fal fa-arrow-left"></i></a>
+	<?php }?>
 
 	<a href="index.php" class="logo">
 		<img src="image/logo.png" alt="">
 		<span><?php echo $config['settings']['sitename_th'];?></span>
 	</a>
-
-	<?php if($current_page == 'home' || $current_page == 'articles'){?>
-	<div class="btn-icon right" id="btn-category"><i class="fal fa-bars"></i></div>
-	<?php }?>
 
 	<?php if($current_page != 'search'){?>
 	<a href="search" class="btn-icon right" title="ค้นหาบทความ"><i class="fal fa-search"></i></a>
