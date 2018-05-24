52b3f605edee5ce9ec10c5ff164c27116cd06f96
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-05-24 18:01:13 +0700

    Fix Navigation Menu

--- template/navigation.php
+++ template/navigation.php
@@ -1,10 +1,13 @@
 <?php $categories = $category->listAll();?>
-<nav>
-	<ul>
-		<?php foreach ($categories as $var) {?>
-		<li><a <?php echo ($var['id'] == $category_id?'class="active"':'');?> href="topic/<?php echo $var['id'];?><?php echo (!empty($var['link'])?'/'.$var['link']:'');?>"><i class="<?php echo ($var['id'] == $category_id?'fa':'fal'); ?> fa-<?php echo (!empty($var['icon'])?$var['icon']:'folder');?>"></i><span><?php echo $var['title'];?></span></a></li>
+<nav class="navigation" id="navigation">
+	<div class="mini-logo">
+		<span><img src="image/logo.png" alt=""></span>
+		<span><?php echo $config['settings']['sitename_th'];?></span>
+	</div>
+	<!-- <a class="nav <?php echo ($current_page == 'home'?'active':'');?>" href="index.php"><i class="<?php echo ($current_page == 'home'?'fa':'fal');?> fa-home"></i>หน้าแรก</a> -->
+	<?php foreach ($categories as $var) {?>
+	<a class="nav <?php echo ($var['id'] == $category_id?'active':''); ?>" href="topic/<?php echo $var['id'];?><?php echo (!empty($var['link'])?'/'.$var['link']:'');?>"><i class="<?php echo ($var['id'] == $category_id?'fa':'fal'); ?> fa-<?php echo (!empty($var['icon'])?$var['icon']:'folder');?>"></i><?php echo $var['title'];?></a>
 	<?php } ?>
-		<li><a <?php echo ($current_page == 'about'?'class="active"':'');?> href="about"><i class="<?php echo ($current_page == 'about'?'fa':'fal');?> fa-info-circle"></i><span>เกี่ยวกับเรา</span></a></li>
-	</ul>
+	<a class="nav <?php echo ($current_page == 'about'?'active':'');?>" href="about"><i class="<?php echo ($current_page == 'about'?'fa':'fal');?> fa-info-circle"></i>เกี่ยวกับเรา</a>
 	<!-- <span>Occmed Prachinburi</span> -->
 </nav>
